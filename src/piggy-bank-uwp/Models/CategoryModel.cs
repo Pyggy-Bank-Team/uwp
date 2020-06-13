0c8bbb0084a355d63f4f334e487cc121d3453d7b
Denis Makarenko <denis-makarenko@outlook.com>
2020-06-13 20:17:47 +0300

    init

---
+++ src/piggy-bank-uwp/Models/CategoryModel.cs
@@ -0,0 +1,13 @@
+﻿namespace piggy_bank_uwp.Models
+{
+    public sealed class CategoryModel : IBaseModel
+    {
+        public string HexColor { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string Id { get ; set; }
+    }
+}
---
+++ src/piggy-bank-uwp/ViewModels/Donate/DonateItemVIewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace piggy_bank_uwp.ViewModels.Donate
+{
+    public class DonateItemViewModel
+    {
+        public string Title { get; set; }
+
+        public string Price { get; set; }
+
+        public string StoreId { get; set; }
+    }
+}
