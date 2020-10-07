a942667e6e1027fab678674bf70dcda3be46cab7
Denis Makarenko <denis-makarenko@outlook.com>
2020-10-07 17:50:07 +0300

    PB-24 Added style for archived categories

--- src/Peppa/Extensions/SolidColorBrushExtension.cs
+++ src/Peppa/Extensions/SolidColorBrushExtension.cs
@@ -5,6 +5,7 @@ namespace Peppa.Extensions
     public static class SolidColorBrushExtension
     {
         public static string ToColor(this SolidColorBrush brush)
+            //TODO Remove alpha from color
             => brush.Color.ToString().Replace("#FF", "#");
     }
 }
