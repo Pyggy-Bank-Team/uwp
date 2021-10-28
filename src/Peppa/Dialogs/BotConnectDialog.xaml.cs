5c889726491667abcd3725457066da2744236c31
Denis Makarenko <denis-makarenko@outlook.com>
2021-10-28 13:36:27 +0300

    PB-111 Added `BotConnectDialog`

---
+++ src/Peppa/Dialogs/BotConnectDialog.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Windows.UI.Xaml.Controls;
+
+
+namespace Peppa.Dialogs
+{
+    public sealed partial class BotConnectDialog : ContentDialog
+    {
+        public BotConnectDialog()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
